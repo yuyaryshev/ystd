d4160e02335ff193b7990f49dac00477fef6b125
Yuri Yaryshev <yuyaryshev@gmail.com>
2021-04-23 20:11:07 +0300

    Fixed filterObject.ts

--- src/filterObject.ts
+++ src/filterObject.ts
@@ -1,11 +1,10 @@
 export function filterObject(object: any, keysOrFilterCallback: string[] | ((key: string, object: any) => boolean), invert?: boolean): any {
     const r = {} as any;
     if (Array.isArray(keysOrFilterCallback)) {
-        for (let key of keysOrFilterCallback) if (invert ? !(key in object) : key in object) r[key] = object[key];
-
+        for (let key in object ) if (invert !== keysOrFilterCallback.includes(key)) r[key] = object[key];
         return r;
     } else {
-        for (let key in object) if (invert ? !keysOrFilterCallback(key, object) : keysOrFilterCallback(key, object)) r[key] = object[key];
+        for (let key in object) if (invert !== keysOrFilterCallback(key, object)) r[key] = object[key];
         return r;
     }
 }
