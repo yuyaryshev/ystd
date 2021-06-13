9f7c9ecd55ab78e9b1f23ed7c3d77b0b7fdc4308
Yuri Yaryshev <yuyaryshev@gmail.com>
2021-06-13 14:39:05 +0300

    Removed ystd_tsdx

--- ystd_tsdx/test/index.test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var index_js_1 = require("../src/index.js");
-describe('sum', function () {
-    it('adds two numbers together', function () {
-        expect((0, index_js_1.sum)(1, 1)).toEqual(2);
-    });
-});
-//# sourceMappingURL=index.test.js.map
--- ystd_tsdx/test/index.test.ts
+++
@@ -1,7 +0,0 @@
-import { sum } from '../src/index.js';
-
-describe('sum', () => {
-  it('adds two numbers together', () => {
-    expect(sum(1, 1)).toEqual(2);
-  });
-});
