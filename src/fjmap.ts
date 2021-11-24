437fbc31d4bd68ca1b8a9f21e3782c8c5e7d6e57
Yuri Yaryshev <yuyaryshev@gmail.com>
2021-11-24 16:56:54 +0300

    - Fixed fmap & fjmap with 'false' - Published 1.9.3

--- src/fjmap.ts
+++ src/fjmap.ts
@@ -1,12 +1,12 @@
 /**
- *
+ * Filters, maps and joins array in one operation
  */
-export function fjmap<T>(array: T[], sep: string, callback: (item: T, index: number) => string | number | undefined | null) {
+export function fjmap<T>(array: T[], sep: string, callback: (item: T, index: number) => string | number | undefined | false | null) {
     const r: string[] = [];
     const len = array.length || 0;
     for (let i = 0; i < len; i++) {
         const itemr = callback(array[i], i);
-        if (itemr !== undefined && itemr !== null && itemr !== "") r.push(itemr + "");
+        if (itemr !== undefined && itemr !== null && itemr !== false && itemr !== "") r.push(itemr + "");
     }
     return r.join(sep);
 }
--- src/fmap.ts
+++ src/fmap.ts
@@ -1,12 +1,12 @@
 /**
- *
+ * Filters & maps array in one operation
  */
-export function fmap<T, R>(array: T[], callback: (item: T, index: number) => R | null | undefined): R[] {
+export function fmap<T, R>(array: T[], callback: (item: T, index: number) => R | null | false | undefined): R[] {
     const r: R[] = [];
     const len = array.length || 0;
     for (let i = 0; i < len; i++) {
         const itemr = callback(array[i], i);
-        if (itemr !== undefined && itemr !== null) r.push(itemr);
+        if (itemr !== undefined && itemr !== null && itemr !== false) r.push(itemr);
     }
     return r;
 }
