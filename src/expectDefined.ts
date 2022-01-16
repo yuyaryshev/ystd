ae28f61ccb0ce5bb8d98bbef250b8ad6e615fbea
Yuri Yaryshev <yuyaryshev@gmail.com>
2022-01-16 13:10:05 +0300

    - Added & fixed expectDefined

--- src/expectDefined.ts
+++ src/expectDefined.ts
@@ -1,4 +1,19 @@
-export function expectDefined<T>(v: T, errorMessage: string): T {
-    if (v === undefined) throw new Error(errorMessage);
+export function expectDefined<T>(v: T | null | undefined, errorMessage?: string): T {
+    if (v === undefined || v === null) {
+        throw new Error(errorMessage || "CODE00000476 Error: expectDefined - failed!");
+    }
     return v;
 }
+
+export function expectDefinedF(errorMessage0: string = "CODE00000287 Error: expectDefined - failed!") {
+    return function expectDefined<T>(v: T | null | undefined, errorMessage: string = errorMessage0): T {
+        if (v === undefined || v === null) {
+            throw new Error(errorMessage || "CODE00000288 Error: expectDefined - failed!");
+        }
+        return v;
+    };
+}
+
+// aliases
+export const throwIfUndefined = expectDefined;
+export const throwIfUndefinedF = expectDefinedF;
--- src/projmeta.ts
+++ src/projmeta.ts
@@ -1,2 +1,2 @@
-export const version = '1.10.0';
+export const version = '1.10.3';
 export const packageName = 'ystd';
