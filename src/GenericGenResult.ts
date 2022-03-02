6deb55011ae0cf515836049bbe4b86c11fe3c55a
Yuri Yaryshev <yuyaryshev@gmail.com>
2022-03-02 11:36:20 +0300

    - Fixed GenericGenResult - Published 2.3.1

--- src/GenericGenResult.ts
+++ src/GenericGenResult.ts
@@ -24,24 +24,66 @@ export interface GenericGenResult {
     [key: string]: unknown[];
 }
 
-export function appendGenResults<T extends GenericGenResult>(targetGenResult: T, ...genResults: T[]): T {
-    for (const sourceGenResult of genResults) {
-        for (let k in sourceGenResult) {
-            const arr = genResults[0][k] || [];
-            genResults[0][k] = arr;
-            arr.push(...sourceGenResult[k]);
-        }
-    }
-    return targetGenResult;
-}
+// export function genResultValueType(v: unknown): "array" | "object" | "string" {
+//     if (Array.isArray(v)) {
+//         return "array";
+//     }
+//
+//     const tp = typeof v;
+//     switch (tp) {
+//         case "object":
+//             if (tp.constructor.name !== "Object") {
+//                 throw new Error(`CODE00000290 GenResult can contain plain-objects only!`);
+//             }
+//             return "object";
+//         case "string":
+//             return tp;
+//     }
+//
+//     throw new Error(`CODE00000291 Unexpected ResultValueType!`);
+// }
+//
+// export function expectMatchingGenResultTypes<T>(a: T | undefined, b: T | undefined) {
+//     const aType = a !== undefined ? genResultValueType(a) : undefined;
+//     const bType = b !== undefined ? genResultValueType(b) : undefined;
+//     if (aType && bType && aType !== bType) {
+//         throw new Error(`CODE000002920 ERROR GenResult type mismatch! ${aType} !== ${bType} `);
+//     }
+//     const r = aType || bType;
+//     if (r === undefined) {
+//         throw new Error(`CODE000002930 ERROR GenResult both arguments are undefined!`);
+//     }
+//     return r;
+// }
 
 export function mergeGenResults<T extends GenericGenResult>(...genResults: T[]): T {
     const targetGenResult = {} as any as T;
     for (const sourceGenResult of genResults) {
         for (let k in sourceGenResult) {
-            const arr = genResults[0][k] || [];
-            genResults[0][k] = arr;
-            arr.push(...sourceGenResult[k]);
+            if (targetGenResult[k] === undefined) {
+                targetGenResult[k] = sourceGenResult[k];
+            } else {
+                if (!Array.isArray(targetGenResult[k])) {
+                    throw Error(
+                        `CODE00000185 mergeGenResults can only merge array fields! But field '${k}' is not an array! Field value: ${JSON.stringify(
+                            targetGenResult[k],
+                        )}`,
+                    );
+                }
+                targetGenResult[k].push(sourceGenResult[k]);
+
+                // switch (expectMatchingGenResultTypes(targetGenResult[k], sourceGenResult[k])) {
+                //     case "array":
+                //         targetGenResult[k].push(sourceGenResult[k]);
+                //         break;
+                //     case "object":
+                //         throw new Error(`CODE00000186 mergeGenResults @notSupported`);
+                //         break;
+                //     case "string":
+                //         throw new Error(`CODE00000187 mergeGenResults @notSupported`);
+                //         break;
+                // }
+            }
         }
     }
     return targetGenResult;
--- src/projmeta.ts
+++ src/projmeta.ts
@@ -1,2 +1,2 @@
-export const version = '2.3.0';
+export const version = '2.3.1';
 export const packageName = 'ystd';
