254049212b2feeca7650cb2827a64ad019dc0f5c
Yuri Yaryshev <yuyaryshev@gmail.com>
2022-03-16 09:05:48 +0300

    - Added removeExpectedPrefix and removeExpectedSuffix - Published 2.4.0

--- src/removePrefix.ts
+++ src/removePrefix.ts
@@ -2,3 +2,10 @@ export const removePrefix = (s: string, prefix: string): string => {
     if (s.startsWith(prefix)) return s.substr(prefix.length);
     return s;
 };
+
+export const removeExpectedPrefix = (s: string, prefix: string): string => {
+    if (!s.startsWith(prefix)) {
+        throw new Error(`CODE00000189 removeExpectedPrefix failed! Expected prefix '${prefix}' in string '${s}'!`);
+    }
+    return s.substr(prefix.length);
+};
--- src/removeSuffix.ts
+++ src/removeSuffix.ts
@@ -2,3 +2,10 @@ export const removeSuffix = (s: string, suffix: string): string => {
     if (s.endsWith(suffix)) return s.substr(0, s.length - suffix.length);
     return s;
 };
+
+export const removeExpectedSuffix = (s: string, suffix: string): string => {
+    if (!s.endsWith(suffix)) {
+        throw new Error(`CODE00000190 removeExpectedSuffix failed! Expected suffix '${suffix}' in string '${s}'!`);
+    }
+    return s.substr(0, s.length - suffix.length);
+};
